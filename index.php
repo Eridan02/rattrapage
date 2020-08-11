cc2d1c27365a622c3aa4148f97ade5fa35da3675
Spaghett02 <marieleblanc.83@gmail.com>
2020-08-11 10:11:56 +0200

    fin mise en place

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
 
-    if (!empty($_FILES)) { ?>
+    if (!empty($_FILES)) {
+
+        echo'<div class="reussite">';
+        print_r("Votre image a été enregistrée! veuillez aller dans la gallerie!");
+        echo'</div>';
+        $img = $_FILES['img'];
+        $ext = strtolower (substr($img['name'], -3));
+        $allow_ext = array("jpg", 'png', 'gif');
+
+        if (in_array($ext, $allow_ext)) {
+            move_uploaded_file($img['tmp_name'], "images/ ".$img['name']);
+        }
+        else
+        $erreur = "erreur";
+    }
+
+
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr"  lang="fr">
@@ -10,7 +27,7 @@
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -30,43 +47,27 @@
   </div>
 </nav>
 <h1>Gallerie interractive!</h1>
-<div class="container">
+<div class="container ">
     <div class="col-sm-1"></div>
-        <div class="col-sm-10">
-            <h2>Formulaire d'ajout du blog</h2>
+    <div class="col-sm-10">
+        <br>
+    <h2>Formulaire d'ajout du blog</h2>
+    <br>
 
-            <form method="post" action="index.php" enctype="multipart/form-data">
-                <div class="form-group col-sm-4">
-                    <input type="file" class="form-control-file" id="img" name="img">
-                </div>
-            <div class="form-group col-sm-4">
-                <input type="submit" name="Envoyer"/>
-            </div>
-       
-            </form>
 
+
+    <form method="post" action="index.php" enctype="multipart/form-data">
+    <div class="form-group col-sm-4">
+    <input type="file" class="form-control-file" id="img" name="img">
     </div>
+    <div class="form-group col-sm-4">
+        <input type="submit" name="Envoyer"/>
+    </div>
+       
+    </form>
+    </div>
+    <div class="col-sm-1"></div>
 </div>
 </body>
 
 </html>
-     
-
-<?php
-    echo'<div class="reussite">';
-       print_r("Votre image a été enregistrée! veuillez aller dans la gallerie!");
-    echo  '</div>';
-        $img = $_FILES['img'];
-        $ext = strtolower (substr($img['name'], -3));
-        $allow_ext = array("jpg", 'png', 'gif');
-
-        if (in_array($ext, $allow_ext)) {
-            move_uploaded_file($img['tmp_name'], "images/ ".$img['name']);
-        }
-        else
-        $erreur = "erreur";
-    }
-
-
-?>
-
